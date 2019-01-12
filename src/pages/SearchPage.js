7a99945b9dcc2972f9e603edfed3223beb7b1e23
Eugen Cazacu <green182day@hotmail.com>
2019-01-12 10:39:41 +0100

    extract searchbar to separate new component

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,6 +3,7 @@ import { context } from '../store/MyContext';
 import { Search } from 'react-feather';
 import { XCircle } from 'react-feather';
 import { Movie } from '../components/Movie'
+import { SearchBar } from '../components/SearchBar'
 
 export default class SearchPage extends Component {
     static contextType = context;
@@ -39,18 +40,17 @@ export default class SearchPage extends Component {
         const movies = this.state.input.length > 0 ? this.context.movies
             .filter((m) => m.title.toUpperCase().includes(this.state.input.toUpperCase()))
             .map((m) => {
-                return <Movie info={m} key={Math.random()}></Movie>;
-            }) : "";
+                return <Movie info={m} key={Math.random()}></Movie>;}) : "";
+
         return (
             <div className="search-bar-page-wrapper" onClick={this.handleClickPage} >
-                <div className="search-bar-top-container">
-                    <div id="search-wrapper" className={this.state.focus === true ? "search-wrapper" : "search-wrapper justify-content-center"} onClick={this.handleClick}>
-                        <Search fill="#333" stroke="grey" height="18" width="18" strokeWidth="3" />
-                        <input onKeyDown={this.handleInput} onInput={this.handleInput} ref={this.textInput} id="input" className={this.state.input.length > 0 ? "" : "input-unfocus"} type="text" placeholder="Search" onFocus={this.handleFocus} onBlur={this.handleBlur} />
-                    </div>
-                    <button onClick={this.handleCancel} id="cancel" className={this.state.init ? "d-none" : this.state.focus ? "d-flex animated slideInRight fastest" : "d-none animated slideOutRight fastest"}>Cancel</button>
-                    <span onClick={this.handleCancel} id="cancel-input" className={!this.state.focus ? !this.state.input.length > 0 ? "d-none" : "cancel-unfocused" : this.state.input.length > 0 ? "" : "d-none"}><XCircle height="18" width="18" color="grey" /></span>
-                </div>
+
+               <SearchBar 
+                    state={this.state} 
+                    handleInput={this.handleInput} 
+                    handleCancel={this.handleCancel} 
+                    handleFocus={this.handleFocus} 
+                    textInput={this.textInput}/>
                 
                 {movies ? <div className="animated fadeIn fast">
                     <div className="search-banner">Movies and TV</div>
